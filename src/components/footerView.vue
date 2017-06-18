d200f98d0f17d94858b04e052e3935c5a98dd3ff
YDZ <ydz627@gmail.com>
2017-06-18 20:38:01 +0800

    封装 footer 组件

---
+++ src/components/footerView.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="footer">
+  <span class="dim">© 2017
+    <a href="https://onevcat.com">OneV's Den</a> &amp;
+    <a href="http://objccn.io/">ObjC 中国</a>
+    <br>本站由
+    <a href="https://halfrost.com">@halfrost</a> 创建
+  </span>
+</div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
