cf5e8bd41625256e46d4dab9a872c22e9429c671
YDZ <ydz627@gmail.com>
2017-06-18 21:05:45 +0800

    封装 products 书籍内容组件

---
+++ src/components/productsDetailInfo/productsContent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="product-content">
+    <h4 class="section-title">本书内容</h4>
+    <div class="product-content-list">
+      <ul>
+        <li v-for="item in contentInfo">
+          <h4>{{item.title}}</h4>
+          <p>{{item.subtitle}}</p>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  props: [
+    'content-info'
+  ]
+}
+</script>
+
+<style>
+
+</style>
