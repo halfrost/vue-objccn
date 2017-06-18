5e911049e0a47b84b55e9e52f795ffbe4d2d8b3a
YDZ <ydz627@gmail.com>
2017-06-18 20:57:33 +0800

    完成全应用的路由设置

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,85 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
-
+import HomePage from '@/pages/homePage'
+import Issues from '@/pages/issues'
+import IssuesDetail from '@/pages/issuesDetail'
+import Products from '@/pages/products'
+import ProductsHome from '@/pages/productsHome'
+import GoodsCart from '@/pages/goodsCart'
+import Login from '@/pages/login'
+import ProductsDetailInfo from '@/pages/productsDetailInfo'
+import ProductsPreview from '@/pages/productsPreview'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  mode: 'history',
+  routes: [{
+    path: '/',
+    name: 'HomePage',
+    component: HomePage
+  },
+  {
+    path: '/issues',
+    name: 'Issues',
+    component: Issues
+  },
+  {
+    path: '/issues/:issuesNum',
+    component: IssuesDetail
+  },
+  {
+    path: '/products',
+    // name: 'Products',
+    component: Products,
+    // redirect: '/products',
+    children: [{
       path: '/',
-      name: 'Hello',
-      component: Hello
+      component: ProductsHome
+    },
+    {
+      path: 'swifter-tips',
+      component: ProductsDetailInfo
+    },
+    {
+      path: 'functional-swift',
+      component: ProductsDetailInfo
+    },
+    {
+      path: 'core-data',
+      component: ProductsDetailInfo
+    },
+    {
+      path: 'advanced-swift',
+      component: ProductsDetailInfo
+    },
+    {
+      path: 'swifter-tips/preview',
+      component: ProductsPreview
+    },
+    {
+      path: 'functional-swift/preview',
+      component: ProductsPreview
+    },
+    {
+      path: 'core-data/preview',
+      component: ProductsPreview
+    },
+    {
+      path: 'advanced-swift/preview',
+      component: ProductsPreview
     }
+    ]
+  },
+  {
+    path: '/goodscart',
+    name: 'GoodsCart',
+    component: GoodsCart
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  }
+
   ]
 })
