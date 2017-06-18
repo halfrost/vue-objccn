6c03e7b436ba18da4c0ea4e2b62d596643baca8d
YDZ <ydz627@gmail.com>
2017-06-18 21:04:31 +0800

    封装 products 常见问题组件

---
+++ src/components/productsDetailInfo/productsProblem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="product-faq">
+    <h4 class="section-title">常见问题</h4>
+    <div class="product-faq-list">
+      <ul>
+        <li v-for="item in problemList">
+          <h4 v-html="item.question"></h4>
+          <p v-html="item.answer"></p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'problem-list'
+  ]
+}
+</script>
+
+<style>
+
+</style>
