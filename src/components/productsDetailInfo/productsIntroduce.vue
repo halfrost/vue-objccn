f6ea153a1ea485012bf0226386c8dfb5db80abcc
YDZ <ydz627@gmail.com>
2017-06-18 21:05:03 +0800

    封装 products 的介绍组件

---
+++ src/components/productsDetailInfo/productsIntroduce.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <h3 class="product-subtitle">{{productSubtitle}}</h3>
+  <div class="product-text">
+    <p v-html="productText"></p>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  props: [
+    'product-subtitle',
+    'product-text'
+  ]
+}
+</script>
+
+<style>
+
+</style>
