e013083832d3545621d48c8ba6b6080b015cc4be
YDZ <ydz627@gmail.com>
2017-06-18 21:03:58 +0800

    封装 products 在线试读组件

---
+++ src/components/productsDetailInfo/productsPreviewTool.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="buy-button-group row">
+  <router-link class="col-sm-3 col-sm-offset-2 col-xs-10 col-xs-offset-1 btn btn-success btn-lg" to="/goodscart"><span class="glyphicon glyphicon-ok"></span>  购买电子书</router-link>
+  <router-link class="col-sm-3 col-sm-offset-2 col-xs-10 col-xs-offset-1 btn btn-default btn-lg" :to="previewUrl + '/preview'"><span class="glyphicon glyphicon-book"></span>  在线试读</router-link>
+</div>
+</template>
+
+<script>
+export default {
+  props: [
+    'preview-url'
+  ],
+  methods: {
+  }
+}
+</script>
+
+<style>
+
+</style>
