27b1b4efbc73335d101fb5de820a749d5f2cc2f2
YDZ <ydz627@gmail.com>
2017-06-18 20:40:51 +0800

    设置 layout 总体页面布局

---
+++ src/components/layout.vue
@@ -0,0 +1,67 @@
+<template>
+<div id="__wrapper">
+
+  <navgation-bar>
+  </navgation-bar>
+
+  <div class="app-content">
+    <keep-alive>
+      <transition name="drop">
+      <router-view></router-view>
+    </transition>
+    </keep-alive>
+  </div>
+
+  <footerView>
+  </footerView>
+
+</div>
+</template>
+
+<script>
+import navgationBar from './navigationBar'
+import bookCard from './bookCard'
+import footerView from './footerView'
+
+export default {
+  components: {
+    navgationBar,
+    bookCard,
+    footerView
+  },
+  created() {
+    this.$store.dispatch('loadUser')
+  },
+  data() {
+    return {
+      isShowAboutDialog: false,
+      isShowLogDialog: false,
+      isShowRegDialog: false,
+      username: '',
+      activeClass: 'navbar-item-select'
+    }
+  },
+  methods: {
+    aboutClick() {
+      this.isShowAboutDialog = true
+    },
+    logClick() {
+      this.isShowLogDialog = true
+    },
+    regClick() {
+      this.isShowRegDialog = true
+    },
+    closeDialog(attr) {
+      this[attr] = false
+    },
+    onSuccessLog(data) {
+
+    }
+  }
+}
+</script>
+
+<style>
+@import '../style/style.css';
+@import '../style/bootstrap-flat.min.css';
+</style>
