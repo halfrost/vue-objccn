f3a623b751ecde49b4a78a93552e33b28acf02c7
YDZ <ydz627@gmail.com>
2017-06-18 20:44:38 +0800

    完成 products 页面的搭建

---
+++ src/pages/products.vue
@@ -0,0 +1,38 @@
+<template>
+<div id="__wrapper">
+  <div class="products-content">
+    <keep-alive>
+      <transition name="slide-right">
+      <router-view></router-view>
+    </transition>
+    </keep-alive>
+  </div>
+</div>
+</template>
+
+<script>
+import productsDetailInfo from './productsDetailInfo'
+import productsHome from './productsHome'
+import productsPreview from './productsPreview'
+
+export default {
+  components: {
+    productsDetailInfo,
+    productsHome,
+    productsPreview
+  },
+  created() {
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+@import '../style/style.css';
+@import '../style/bootstrap-flat.min.css';
+</style>
