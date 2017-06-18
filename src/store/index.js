7dac995747758bfac8c2e254fb0d4ec718cdbce4
YDZ <ydz627@gmail.com>
2017-06-18 20:33:46 +0800

    引入 Vuex 单向数据流

---
+++ src/store/index.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import lowdb from 'lowdb'
+
+Vue.use(Vuex)
+
+const db = lowdb('db')
+
+const user = {}
+
+db.defaults({ user }).write()
+
+export default new Vuex.Store({
+  state: {
+    user: {}
+  },
+  actions: {
+    loadUser(context) {
+      const user = db.get('user').value()
+      context.commit('setUser', user)
+      return Promise.resolve()
+    }
+  },
+  mutations: {
+    setUser(state, user) {
+      state.user = user
+    },
+    userLogin(state, user) {
+      state.user = user
+      db.set('user.email', user.email).write()
+      db.set('user.isLogin', true).write()
+    },
+    userLogout(state) {
+      state.user.isLogin = false
+      db.set('user.isLogin', false).write()
+    }
+  }
+})
