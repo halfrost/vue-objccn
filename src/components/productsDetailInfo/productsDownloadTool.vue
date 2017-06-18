ea1348ebc086a8b7a576f6b7c6e67c6a6ed1730e
YDZ <ydz627@gmail.com>
2017-06-18 21:01:31 +0800

    封装 products 下载工具组件的封装，暂时不显示，以后可能会考虑 v-show

---
+++ src/components/productsDetailInfo/productsDownloadTool.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="buy-button-group row">
+    <div class="btn-group btn-group-lg btn-group-justified" role="group">
+      <a class="btn btn-default" role="button" href="">PDF 格式</a>
+      <a class="btn btn-default" role="button" href="">mobi 格式</a>
+      <a class="btn btn-default" role="button" href="">epub 格式</a>
+      <a class="btn btn-default" role="button" href="">打包下载</a>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
